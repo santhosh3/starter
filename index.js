b68d4bb920b0f84928e8f762ea911a79c0e7b990
santosh.srinivasulu <santosh.srinivasulu@roi.actcorp.in>
2025-08-19 20:31:22 +0530

    class2

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log('this is my first JS file');
+// alert('This is alter message box');
+
+
+/**
+ * this is multi line comment
+ * 
+ * 
+ * dfzkjnvkjf
+ * difbdzfbn
+ * 
+ */
+
+let thisName = "abcd"
+
+const myName = "xcvb";
+
+
+console.log(thisName)
