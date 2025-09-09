de158a946bcba27c1d1cd6f400473bbd0e8a0c5d
santosh.srinivasulu <santosh.srinivasulu@roi.actcorp.in>
2025-09-09 19:20:26 +0530

    react-start

---
+++ react_start/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Usestate from './components/Usestate';
+import TrackingMap from './components/Leaflet';
+
+function App() {
+  return (
+      <div>
+        <TrackingMap/> 
+      </div>
+  );
+}
+
+export default App;
---
+++ uber/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server, { cors: { origin: "*" } });
+
+// 🔑 get real route using OSRM (open source)
+async function getRoute(start, end) {
+  try {
+  const url = `https://router.project-osrm.org/route/v1/driving/${start.lng},${start.lat};${end.lng},${end.lat}?overview=full&geometries=geojson`;
+  console.log(url);
+  const res = await axios.get(url);
+  const coords = res.data.routes[0].geometry.coordinates;
+  console.log(coords)
+  // convert [lng, lat] → {lat, lng}
+  return coords.map(([lng, lat]) => ({ lat, lng }));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("startRide", async ({ start, end }) => {
+    console.log("Ride started:", start, "→", end);
+
+    try {
+      const path = await getRoute(start, end);
+      let index = 0;
+
+      const interval = setInterval(() => {
+        if (index >= path.length) {
+          clearInterval(interval);
+          socket.emit("rideComplete");
+          return;
+        }
+        socket.emit("locationUpdate", { current: path[index], path });
+        index++;
+      }, 1000); // 1 update per second
+    } catch (err) {
+      console.error("Route error:", err.message);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(4000, () => console.log("🚀 Server running on port 4000"));
